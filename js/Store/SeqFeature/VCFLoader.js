14789ca45e68b934f7c4ededa69115b1458b8e2e
Colin <colin.diesh@gmail.com>
2016-04-21 16:41:56 -0500

    Use DeferredFeaturesMixin

--- js/Store/SeqFeature/VCFLoader.js
+++ js/Store/SeqFeature/VCFLoader.js
@@ -4,7 +4,8 @@ define([
            'dojo/request/xhr',
            'dojo/Deferred',
            'JBrowse/Store/SeqFeature/VCFTabix',
-           'JBrowse/Store/SeqFeature'
+           'JBrowse/Store/SeqFeature',
+           'JBrowse/Store/DeferredFeaturesMixin'
        ],
        function(
            declare,
@@ -12,31 +13,40 @@ define([
            xhr,
            Deferred,
            VCFTabix,
-           SeqFeatureStore
+           SeqFeatureStore,
+           DeferredFeaturesMixin
        ) {
 
 
-return declare( SeqFeatureStore,
+return declare( [SeqFeatureStore, DeferredFeaturesMixin],
 {
     constructor: function( args ) {
         var thisB = this;
+        console.log('here');
         var tbiUrl = this.resolveUrl( this.getConf('tbiUrlTemplate',[]) || this.getConf('urlTemplate',[])+'.tbi' );
         xhr(tbiUrl).then(function(ret) {
             thisB.store = new VCFTabix(args);
+            thisB.store._deferred.features.then(function() {
+                thisB._deferred.features.resolve({success: true});
+            });
         }, function(err) {
             if(err.response.status == 404) {
                 console.log('Tabix not found, using blank');
             }
+            thisB._deferred.features.resolve({success: true});
         });
     },
-    getFeatures: function(query, featCallback, successCallback, errorCallback) {
+    _getFeatures: function(query, featCallback, successCallback, errorCallback) {
         if(this.store) {
             return this.store.getFeatures(query, featCallback, successCallback, errorCallback);
         }
         else successCallback({});
     },
     getVCFHeader: function() {
-        return this.store.getVCFHeader();
+        if(this.store) {
+            return this.store.getVCFHeader();
+        }
+        else return {};
     }
 });
 });
